4d7526da5095236c2b2b55c0c93a123de1e0e4a9
carlosedupm <carlosedupm@gmail.com>
2021-11-01 19:11:11 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from 'components/DataTable/DataTable';
+import Footer from 'components/Footer/Footer';
+import NavBar from 'components/NavBar/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
